d48c901e872f94c265c3c27f1d8cfd9cb6e401d2
NickChlam <Nick.Chlam@rht.com>
2017-10-16 10:19:08 -0600

    recipie list and recipie model

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
+
 
 @Component({
   selector: 'app-recipie-list',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
+  recipies: Recipie[] = [
+    new Recipie('a test recipie', 'This is a test desc', 
+    'http://www.seriouseats.com/images/2016/12/20161226-staff-picks-best-recipes-2016-chicken-with-cabbage.jpg')];
 
   constructor() { }
 
